e844c6ac98922b84bdcf4efbe5af5cc0dd472f56
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-02 15:47:42 +0300

    moved wine routes to wine.js file

---
+++ routes/wines.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination : './wines/',
+    filename : (req, file, callback) => {
+        callback(null, Date.now() + '-' + file.originalname) 
+    }
+})
+
+const upload = multer({storage : storage})
+
+const router  =express.Router()
+
+router.post('/', upload.single('wineImage'),(req, res) => {
+    const { winePrice, wineName, wineDescription } = req.body
+    const { filename } = req.file
+
+    
+    const sql = `INSERT INTO wine (price, name, description, filename) VALUES (?, ?, ?, ?)`
+    
+    connection.query(
+             sql, 
+             [winePrice, wineName, wineDescription, filename ],
+             (error, results) => {
+                if (error) {
+                    res.send(error)
+                } else {
+                    res.send({message: 'Product saved successfully'})
+                }
+             }
+        )
+})
+
+router.get('/:filename', (req,res) => {
+    const { filename } = req.params
+    res.sendFile(filename, {root : './wines/'})
+})
+
+router.get('/', (req, res) => {
+    const sql = `SELECT * FROM wine ORDER BY price`
+
+    connection.query(
+        sql,
+        (error, results) => {
+            if (error) {
+                res.send(error)
+            } else {
+                res.send(results)
+            }
+        }
+    )
+})
+
+module.exports = router
