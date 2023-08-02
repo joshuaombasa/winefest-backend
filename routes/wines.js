3658c493dcde89f65f1914f07b04ad17363a807f
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-02 20:05:29 +0300

    moved business logic to controller folder

--- routes/wines.js
+++ routes/wines.js
@@ -1,14 +1,5 @@
 const express = require('express')
 const multer = require('multer')
-const mysql = require('mysql')
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'winefest'
-})
-
 
 const storage = multer.diskStorage({
     destination : './wines/',
@@ -21,63 +12,13 @@ const upload = multer({storage : storage})
 
 const router  =express.Router()
 
-router.post('/', upload.single('wineImage'),(req, res) => {
-    const { winePrice, wineName, wineDescription } = req.body
-    const { filename } = req.file
+const winesCtrl = require('../controllers/winesController')
 
-    
-    const sql = `INSERT INTO wine (price, name, description, filename) VALUES (?, ?, ?, ?)`
-    
-    connection.query(
-             sql, 
-             [winePrice, wineName, wineDescription, filename ],
-             (error, results) => {
-                if (error) {
-                    res.send(error)
-                } else {
-                    res.send({message: 'Product saved successfully'})
-                }
-             }
-        )
-})
 
-router.get('/:filename', (req,res) => {
-    const { filename } = req.params
-    res.sendFile(filename, {root : './wines/'})
-})
 
-router.get('/', (req, res) => {
-    const sql = `SELECT * FROM wine ORDER BY price`
-
-    connection.query(
-        sql,
-        (error, results) => {
-            if (error) {
-                res.send(error)
-            } else {
-                res.send(results)
-            }
-        }
-    )
-})
-
-router.delete('/:id', (req, res) => {
-
-    const {id} = req.params
-
-    const sql = 'DELETE FROM wine WHERE ID = ?'
-
-    connection.query(
-        sql,
-        [parseInt(id)],
-        (error, results) => {
-            if (error) {
-                res.json(error)
-            } else {
-                res.json({message: 'Wine deleted successfully!'})
-            }
-        }
-    )
-})
+router.get('/:filename', winesCtrl.getWineImage)
+router.post('/', upload.single('wineImage'), winesCtrl.addNewWine)
+router.get('/', winesCtrl.getAllWines)
+router.delete('/:id', winesCtrl.deleteSingleWine)
 
 module.exports = router
