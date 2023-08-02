6dd8d79f29a42df5cf27b8dd09984fec2da3dd5f
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-02 15:48:08 +0300

    moved order routes to orders.js file

---
+++ routes/orders.js
@@ -0,0 +1,57 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.post('/', (req, res) => {
+    const { product_id, price, name,filename } = req.body
+ 
+     const sql = `INSERT INTO orders (id, price, name, filename) VALUES (?, ?, ?, ?)`
+ 
+     connection.query(
+         sql,
+         [product_id, price, name, filename],
+         (error, results) => {
+             if (error) {
+                 res.send(error)
+             } else {
+                 res.send({message : `${name} added to cart successfully`})
+             }
+         }
+     )
+ })
+ 
+ router.get('/', (req, res) => {
+     const sql = `SELECT * FROM orders`
+ 
+     connection.query(
+         sql,
+         (error, results) => {
+             if (error) {
+                 res.send(error) 
+             } else {
+                 res.send(results)
+             }
+         }
+ 
+     )
+ })
+ 
+ router.delete('/:id', (req, res) => {
+     const {id} = req.params
+ 
+     const sql = `DELETE FROM orders WHERE id = ?`
+ 
+     connection.query(
+         sql,
+         [parseInt(id)],
+         (error, results) => {
+             if (error) {
+                 console.log(error)
+             } else {
+                 res.send(results)
+             }
+         }
+     )
+ })
+
+module.exports  =router
