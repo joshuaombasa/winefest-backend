1118f583d003eb9dc3b63e30faa85a10ee567fb2
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-02 15:47:09 +0300

    moved routing logic to routes foler

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql')
-const multer = require('multer')
-// const { message } = require('statuses')
+
+const wineRoutes = require('./routes/wines')
+const orderRoutes = require('./routes/orders')
 const app = express()
 
  const connection = mysql.createConnection({
@@ -12,14 +13,7 @@ const app = express()
     database: 'winefest'
 })
 
-const storage = multer.diskStorage({
-    destination : './wines/',
-    filename : (req, file, callback) => {
-        callback(null, Date.now() + '-' + file.originalname) 
-    }
-})
 
-const upload = multer({storage : storage})
 
 app.use(cors())
 
@@ -27,97 +21,9 @@ app.use(express.json())
 
 // app.use(express.urlencoded({extended: false}))
 
-app.post('/wines', upload.single('wineImage'),(req, res) => {
-    const { winePrice, wineName, wineDescription } = req.body
-    const { filename } = req.file
+app.use('/wines', wineRoutes)
 
-    
-    const sql = `INSERT INTO wine (price, name, description, filename) VALUES (?, ?, ?, ?)`
-    
-    connection.query(
-             sql, 
-             [winePrice, wineName, wineDescription, filename ],
-             (error, results) => {
-                if (error) {
-                    res.send(error)
-                } else {
-                    res.send({message: 'Product saved successfully'})
-                }
-             }
-        )
-})
-
-app.get('/wines/:filename', (req,res) => {
-    const { filename } = req.params
-    res.sendFile(filename, {root : './wines/'})
-})
-
-app.get('/wines', (req, res) => {
-    const sql = `SELECT * FROM wine ORDER BY price`
-
-    connection.query(
-        sql,
-        (error, results) => {
-            if (error) {
-                res.send(error)
-            } else {
-                res.send(results)
-            }
-        }
-    )
-})
-
-app.post('/orders', (req, res) => {
-   const { product_id, price, name,filename } = req.body
-
-    const sql = `INSERT INTO orders (id, price, name, filename) VALUES (?, ?, ?, ?)`
-
-    connection.query(
-        sql,
-        [product_id, price, name, filename],
-        (error, results) => {
-            if (error) {
-                res.send(error)
-            } else {
-                res.send({message : `${name} added to cart successfully`})
-            }
-        }
-    )
-})
-
-app.get('/orders', (req, res) => {
-    const sql = `SELECT * FROM orders`
-
-    connection.query(
-        sql,
-        (error, results) => {
-            if (error) {
-                res.send(error) 
-            } else {
-                res.send(results)
-            }
-        }
-
-    )
-})
-
-app.delete('/orders/:id', (req, res) => {
-    const {id} = req.params
-
-    const sql = `DELETE FROM orders WHERE id = ?`
-
-    connection.query(
-        sql,
-        [parseInt(id)],
-        (error, results) => {
-            if (error) {
-                console.log(error)
-            } else {
-                res.send(results)
-            }
-        }
-    )
-})
+app.use('/orders', orderRoutes)
 
 app.listen(3000, () => {
     console.log('App listening on port 3000')
