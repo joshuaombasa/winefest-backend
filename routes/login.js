7d25a1c58896958601f73001acbae2dff74adb2e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-08 16:44:10 +0300

    added logic to send back jwt token

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const { check, validationResult} = require('express-validator')
 const router = express.Router()
 const loginCtrl = require('../controllers/loginController')
-router.post('/', loginCtrl.loginDealer)
+
+router.post('/', [
+    check("email", "Please provide a valid email").isEmail(),
+    check("password", "Please provide a password greater than 7 characters").isLength({min: 8})
+], loginCtrl.loginDealer)
 
 module.exports = router
